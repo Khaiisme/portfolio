4c1b533c7d7f294355fcfe6218b3ce9a1130f1a0
Van Khai <kn579309@gmail.com>
2025-02-08 04:07:19 +0100

    .

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,13 +2,13 @@ import Navbar from "./navbar";
 const Header = ({ opacity }) => {
   return (
     <header
-      className="fixed top-0 w-full bg-transparent text-white transition-opacity duration-500 "
+      className="fixed top-0 w-full mx-1 bg-transparent text-white transition-opacity duration-500 "
       style={{ opacity }}
     >
-      <nav className="max-w mx-auto px-10 py-4 flex items-center justify-between ">
+      <nav className="max-w px-10 py-4 flex items-center justify-between ">
         <div>
           <button
-            className="bg-white text-black text-sm font-bold px-4 py-2 rounded-md shadow hover:bg-gray-200 transition ml-40 sm:ml-auto"
+            className="bg-white text-black text-sm font-bold px-4 py-2 rounded-md shadow hover:bg-gray-200 transition ml-52 sm:ml-auto"
             onClick={() =>
               document
                 .querySelector("#contact")
